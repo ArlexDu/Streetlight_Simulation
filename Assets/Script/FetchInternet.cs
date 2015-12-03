8825d370b8e56ad594ae1626399057fed03cb0b9
Happyfine <772773671@qq.com>
2015-12-03 22:19:43 +0800

    与服务器连接，解析json数组，改变灯的状态

---
+++ Assets/Script/FetchInternet.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+public class FetchInternet : MonoBehaviour {
+
+	WWW http;
+	string url;
+	private float lastbright;
+	// Use this for initialization
+	void Start () {
+		url = "http://10.60.42.49:8083/light/getOne?id=1";
+		http = new WWW (url);
+		StartCoroutine (WaitforRequest(http));
+		InvokeRepeating("getInfo",2,0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void getInfo(){
+		StartCoroutine (WaitforRequest(http));
+	}
+	/*
+	 * json 格式：
+	 * {
+	 * "color" : "000050",
+	 * "bright":  "1.5",
+	 * "id"    :  2
+ 	 * }
+	 * 
+	 */
+
+	IEnumerator WaitforRequest(WWW www){
+		yield return www;
+		if (www.error != null) {
+			Debug.Log ("failed! "+ www.error);
+		} else {
+			if(www.isDone){
+		//		Debug.Log ("succeed!");
+				JsonData data = JsonMapper.ToObject(www.text);
+				string bright = (string)data["bright"];
+		//		Debug.Log("bright is "+ bright);
+				//改变灯光的强度
+				if(lastbright!=float.Parse(bright)){
+					GameObject.Find("Lighting").GetComponent<ControlLighting>().changeIntensity(float.Parse(bright));
+					lastbright = float.Parse(bright);
+				}
+			}
+		}
+	}
+}
--- Assets/Script/Light/ControlLighting.cs
+++ Assets/Script/Light/ControlLighting.cs
@@ -3,44 +3,73 @@ using System.Collections;
 
 public class ControlLighting : MonoBehaviour {
 
-	public GameObject[] Lighting=new GameObject[4];//正对着的light,右边的light,左边的light,后面的light
-	private float Light_intensity; // 存储当前的光强
+	private GameObject[] Lighting=new GameObject[6];//正对着的light,右边的light,左边的light,后面的light
+	private float Light_intensity ; // 存储当前的光强
+	private float old_intensity; // 存储上一次变化的光强
 	private Color start_Color;
 	private Color end_Color;
+	private int max_lights;
 	private float max_lighting = 3;//最大的光照强度
 	private ArrayList Colors = new ArrayList(){new Color(0.8f,0.8f,0.8f,0.8f),new Color(1f,0,0,1f),new Color(0,1f,0,1f),new Color(0,0,1f,1f)};
 	private int current_color = 0;
 	// Use this for initialization
 	void Start () {
-		Light_intensity = Lighting [0].GetComponent<Light> ().intensity;
-		//Debug.Log ("color is :"+Lighting [0].GetComponent<Light> ().color);
+		Debug.Log ("parent name is :"+transform.parent.parent.name);
+		if (transform.parent.parent.name == "ShowLight4") {
+			max_lights = 6;
+		} else {
+			max_lights = 4;
+		}
+		for (int i = 0; i < max_lights; i++) {
+			Lighting[i] = transform.GetChild(i).gameObject;
+		}
 		start_Color = Color.white;
 		end_Color = Color.red;
+		Light_intensity = Lighting [0].GetComponent<Light> ().intensity;
+		old_intensity = 1f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	  //点击上箭头光强增加
-	  if (Input.GetKey (KeyCode.UpArrow)) {
+	    //点击上箭头光强增加
+		//测试专用
+	   /*if (Input.GetKey (KeyCode.UpArrow)) {
 			ChangeLightIntensity (0.05f);
 		} else if (Input.GetKey (KeyCode.DownArrow)) {//点击下箭头光强减少
 			ChangeLightIntensity (-0.05f);
+		}*/
+
+		if (old_intensity < Light_intensity) {
+			Debug.Log ("change up");
+			ChangeLightIntensity (0.05f);
+		} else if(old_intensity > Light_intensity){
+			ChangeLightIntensity (-0.05f);
+			Debug.Log ("change down");
 		}
 	}
 
 
 	//改变光照的强度
 	private void ChangeLightIntensity(float inten){
-		Light_intensity += inten;
-		if (Light_intensity > max_lighting) {
-			Light_intensity = max_lighting;
+		old_intensity += inten;
+		if (old_intensity > max_lighting) {
+			old_intensity = max_lighting;
 		} else if (Light_intensity < 0) {
 			Light_intensity = 0;
 		}
-		for (int i=0; i<4; i++) {
-			Lighting[i].GetComponent<Light>().intensity = Light_intensity;
+		for (int i=0; i<max_lights; i++) {
+			Lighting[i].GetComponent<Light>().intensity = old_intensity;
 		}
-
+		if ((old_intensity >= Light_intensity) && (inten>0)) {
+			old_intensity = Light_intensity;
+		}else if((old_intensity <= Light_intensity) && (inten<0)){
+			old_intensity = Light_intensity;
+		}
 	}
+
+	public void changeIntensity(float i){
+		Light_intensity = i;
+	}
+
 	
 }
